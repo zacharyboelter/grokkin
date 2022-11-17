4721ae1b785b192950b1514e8e93aa51e10ff0b0
Zachary Boelter <zboelter@gmail.com>
2022-11-17 12:24:41 -0800

    binary_index_search

--- index.py
+++ index.py
@@ -1,1 +1,26 @@
 print('hello world')
+
+# Binary Search
+
+def binary_search(list, item):
+
+    low = 0
+    high = len(list) - 1
+
+    while low <= high:
+        mid = (low + high)
+        guess = list[mid]
+        if guess == item:
+            return mid
+        if guess > item:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return None    
+
+my_list = [1, 3, 6, 15, 29, 50]
+
+print(binary_search(my_list, 3))
+print(binary_search(my_list, 29))
+print(binary_search(my_list, 50))
+
