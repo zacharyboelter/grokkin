6e65c0abbb2492c8256415a0dde8338721cb7d9c
Zachary Boelter <zboelter@gmail.com>
2023-05-06 15:18:05 -0700

    if/else

--- index.py
+++ index.py
@@ -27,3 +27,16 @@ print(binary_search(my_list, 50))
 
 # okay, i am back. Need to make sure and code for 1 hour a day
 
+# Alex just got a new hula hoop, he loves it but feels discouraged because his little brother is better than him
+
+# Write a program where Alex can input (n) how many times the hoop goes round and it will return him an encouraging message :)
+
+#     If Alex gets 10 or more hoops, return the string "Great, now move on to tricks".
+#     If he doesn't get 10 hoops, return the string "Keep at it until you get it".
+
+def hoopCounts(n):
+    if n >= 10:
+        print('Great work')
+    else:
+        print('Nope fucko, keep trying.')
+        
