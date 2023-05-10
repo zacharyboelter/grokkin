5826d00f4512738c9d3499e8a66813659e53254d
Zachary Boelter <zboelter@gmail.com>
2023-05-09 19:08:33 -0700

    reverse words in string function

--- index.py
+++ index.py
@@ -52,3 +52,20 @@ def hoopCounts(n):
 def add_five(num):
     total = num + 5
     return total
+
+
+# Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+# Examples
+
+# "This is an example!" ==> "sihT si na !elpmaxe"
+# "double  spaces"      ==> "elbuod  secaps"
+
+
+def reverse_words(text):
+    
+    words = text.split(" ")
+    for i in range(len(words)):
+        words[i] = words[i][::-1]
+    return " ".join(words)
+    
+    
