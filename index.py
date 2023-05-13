a44555ebdc4ba88860c9d84fd29e800c5e9de37f
Zachary Boelter <zboelter@gmail.com>
2023-05-13 10:56:22 -0700

    boolean to yes/no

--- index.py
+++ index.py
@@ -80,3 +80,12 @@ def reverse_words(text):
 def solution(string, ending):
     return string.endswith(ending)
     pass
+
+
+# Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+def bool_to_word(boolean):
+    if boolean:
+        return 'Yes'
+    else:
+        return 'No'
