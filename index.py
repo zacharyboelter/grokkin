3500b586b995aec566caecebda5dd1504f462ed5
Zachary Boelter <zboelter@gmail.com>
2023-05-13 10:49:25 -0700

    git commit -m

--- index.py
+++ index.py
@@ -68,4 +68,15 @@ def reverse_words(text):
         words[i] = words[i][::-1]
     return " ".join(words)
     
-    
+
+
+# Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+# Examples:
+
+# solution('abc', 'bc') # returns true
+# solution('abc', 'd') # returns false
+
+def solution(string, ending):
+    return string.endswith(ending)
+    pass
